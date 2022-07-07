8c077f032e803d72588e8d836506e525467dd002
Alice Chen <104160346+chenalice420@users.noreply.github.com>
2022-07-07 04:40:25 -0700

    Update start.sh

--- ayaya3/config/polybar/start.sh
+++ ayaya3/config/polybar/start.sh
@@ -1,3 +1,9 @@
 !#/bin/sh
-
-polybar mybar
+#polybar mybar
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload mybar &
+  done
+else
+  polybar --reload mybar &
+fi
